39e12703fed8dc76790e9010275c16f430af0941
jacob-security <jahcub444@gmail.com>
2026-02-08 07:12:01 -0600

    wrote various functions

--- db_manager.py
+++ db_manager.py
@@ -26,8 +26,7 @@ def initialize_db() -> None:
             current_scan_query = '''
                 CREATE TABLE IF NOT EXISTS current_scan(
                     path TEXT PRIMARY KEY,
-                    hash TEXT,
-                    last_seen DATETIME DEFAULT CURRENT_TIMESTAMP
+                    hash TEXT
                 )
             '''
             
@@ -49,20 +48,111 @@ def initialize_db() -> None:
             traceback.print_exc()
 
 
-
+# This not only populates but also updates the baseline table
 def populate_baseline(hash_list) -> None:
-    pass
+    with sqlite3.connect(DB_PATH) as connection:
+        try:
+            print("--- Populating baseline table ---")
+            cursor = connection.cursor()
+
+            query = '''
+                INSERT INTO baseline (path, hash)
+                VALUES (?, ?)
+                ON CONFLICT(path)
+                DO UPDATE SET
+                    hash = excluded.hash,
+                    last_seen = CURRENT_TIMESTAMP;
+            '''
+            
+            cursor.executemany(query, hash_list)
+            connection.commit()
+            print("[+] Success: baseline table populated")
+        except Exception as e:
+            print(f"[-] Error populating baseline table: {e}")
+            traceback.print_exc()
+
 
 def populate_current_scan(hash_list) -> None:
-    pass
+    with sqlite3.connect(DB_PATH) as connection:
+        try:
+            print("--- Populating current_scan table ---")
+            cursor = connection.cursor()
+            
+            # Clear the current scan table before populating
+            cursor.execute("DELETE FROM current_scan")
 
-def populate_paths_watched(path) ->
+            query = '''
+                INSERT INTO current_scan (path, hash)
+                VALUES (?, ?)
+            '''
 
-def diff_check() -> None:
-    pass
+            cursor.executemany(query, hash_list)
+            connection.commit()
+        except Exception as e:
+            print(f"[-] Error populating current_scan table: {e}")
+            traceback.print_exc()
+
+
+
+def populate_paths_watched(path) -> None:
+
+    full_path = os.path.abspath(os.path.expanduser(path))
+    with sqlite3.connect(DB_PATH) as connection:
+        try:
+            print("--- Populating paths_watched table ---")
+            cursor = connection.cursor()
+            
+            query = '''
+                INSERT INTO paths_watched (path, last_seen)
+                VALUES (?, CURRENT_TIMESTAMP)
+                ON CONFLICT(path)
+                DO UPDATE SET last_seen = CURRENT_TIMESTAMP
+            '''
+
+            cursor.execute(query, (full_path,))
+            connection.commit()
+            print("[+] Success: populated paths_watched table")
+        except Exception as e:
+            print(f"[-] Error populating paths_watched table {e}")
+
+
+
+def detect_modifications() -> list:
+    with sqlite3.connect(DB_PATH) as connection:
+        try:
+            print("--- Detecting Modifications ---")
+            cursor = connection.cursor()
+
+            query = '''
+                SELECT b.path
+                FROM baseline as b
+                JOIN current_scan c USING(path)
+                WHERE b.hash != c.hash
+            '''
+
+            cursor.execute(query)
+
+            print("[+] Success detecting modifications")
+            modified_list = cursor.fetchall()
+            if modified_list:
+                print("[!] The following files were modified:")
+                for item in modified_list:
+                    print(item)
+                return(modified_list)
+            else:
+                print("[+] No modifications detected")
+                return []
+        except Exception as e:
+            print(f"[-] Error detecting modifications {e}")
 
 def main():
-    initialize_db()
+    test_path = "./test_directory" 
+    #initialize_db()
+    hash_list = scan(test_path)
+    #populate_baseline(hash_list)
+    populate_current_scan(hash_list)
+    populate_paths_watched(test_path)
+    detect_modifications()
 
 if __name__ == "__main__":
     main()
